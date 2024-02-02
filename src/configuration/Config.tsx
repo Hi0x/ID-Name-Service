0c4fc1c7a6a22d8cd005d39076210a37ccf51814
Hi0x <hi@hi0x.net>
2024-02-02 09:41:01 +0200

    Update Config.tsx

--- src/configuration/Config.tsx
+++ src/configuration/Config.tsx
@@ -1,9 +1,9 @@
 import { ThemingProps } from '@chakra-ui/react'
 import { polygon} from '@wagmi/chains'
 
-export const SITE_NAME = '0X Domain Name'
+export const SITE_NAME = 'ID Domain Name'
 export const SITE_DESCRIPTION = 'Hi0x © 2024'
-export const SITE_URL = 'https://0xns.pro'
+export const SITE_URL = 'https://id.0xns.pro'
 
 export const THEME_INITIAL_COLOR = 'system'
 export const THEME_COLOR_SCHEME: ThemingProps['colorScheme'] = 'gray'
@@ -16,20 +16,20 @@ export const SOCIAL_LINKEDIN = ''
 export const SOCIAL_DISCORD = ''
 
 export const NETWORKS = [polygon]; //polygon, filecoin, polygonMumbai
-export const NETWORK_ERROR = "Connect your wallet/Change network to polygon !" //Change network name as required
+export const NETWORK_ERROR = "Connect your wallet" //Change network name as required
 
-export const DOMAIN_TLD = '0x' //primary domain name without dot (.)
-export const DOMAIN_PRICE_ETH = '1' //price should be equal to contract or higher 
-export const DOMAIN_IMAGE_URL = 'https://ipfs.io/ipfs/QmWfLXhfseaSkLrGm14my1TyrKRZ1H8W4DXtvpN9yWxYo9' //Image path starts with ipfs:// or https://
+export const DOMAIN_TLD = 'id' //primary domain name without dot (.)
+export const DOMAIN_PRICE_ETH = '5' //price should be equal to contract or higher 
+export const DOMAIN_IMAGE_URL = 'https://ipfs.io/ipfs/QmUn6ixnBwbc28T1wfmMVuM8zEFUhENaumvKgYYJQnX6wk' //Image path starts with ipfs:// or https://
 export const DOMAIN_NETWORK_CHAIN = 137 //137 for polygon, 314 for filecoin, 80001 form mumbai
-export const DOMAIN_DESCRIPTION = 'Decentralized websites, business cards, personal ID, Register a new domain: https://0xns.pro'
+export const DOMAIN_DESCRIPTION = 'Web3 Domain Name'
 export const DOMAIN_TYPE = "W3D" //W3D for polygon, FVM for Filecoin net
 export const DOMAIN_TITLE = "" //Title above the search input field. 
-export const DOMAIN_PLACEHOLDER = "Search for a name" //Placeholder for search input field 
+export const DOMAIN_PLACEHOLDER = "Search for ID ... Mint price 5 MATIC" //Placeholder for search input field 
 
-export const ADMIN_WALLET = "0x6ec0Ce3333A7A2a37e38c656B46752F2a9fC8e19" //ETH wallet address 
+export const ADMIN_WALLET = "0xbed79816b54E75eD54BF217333342C8d271b3b6f" //ETH wallet address 
 
 export const NOTICE_TITLE = "Airdrop"
-export const NOTICE_NON_MEMBER = "Become a .0x holder to unlock"
+export const NOTICE_NON_MEMBER = "Become a .ID holder to unlock"
 
-export const DOMAIN_BANNER = "https://i.imgur.com/gG8OTOa.png" //290x80 size
+export const DOMAIN_BANNER = "https://i.imgur.com/Ky8J4NM.png" //290x80 size
