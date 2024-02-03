717d5014df3add4d14b8b148d95c7330a6704cc1
Hi0x <hi@hi0x.net>
2024-02-03 19:45:55 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ const Home: NextPage = () => {
     <Flex
       align="center"
       justify="center"
-      bg={useColorModeValue("white", "gray.700")}
+      bg={"#fff0"}
       borderRadius="md"
       color={useColorModeValue("gray.700", "whiteAlpha.900")}
       shadow="base"
